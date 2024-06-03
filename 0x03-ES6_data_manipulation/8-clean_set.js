603ff12911d825f6dea266abad8440a39d7d3bf4
DouibaOmar <douibaomar40@gmail.com>
2024-06-03 18:47:45 +0100

    Done

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
-  let str = [];
-  if (startString === "") {
-    return "";
+  const str = [];
+  if (startString === '') {
+    return '';
   }
   for (const name of set) {
     if (name.startsWith(startString)) {
       str.push(name.slice(startString.length));
     }
   }
-  return str.join("-");
+  return str.join('-');
 }
