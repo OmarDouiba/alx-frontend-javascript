24cc79c035924bdb70a6dd3a17690a1fb20bd350
DouibaOmar <douibaomar40@gmail.com>
2024-06-03 19:02:18 +0100

    Done

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -4,6 +4,6 @@ export default function groceriesList(name, quantity) {
     map.set('Tomatoes', 10)
     map.set('Pasta', 1)
     map.set('Rice', 1)
-    map.set('Banana', 1)
+    map.set('Banana', 5)
     return map
 }
